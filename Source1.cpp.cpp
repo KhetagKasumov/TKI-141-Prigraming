f6b6913c81b3f23662e6776a41d81569dc210c9e
KhetagKasumov <113605412+KhetagKasumov@users.noreply.github.com>
2022-10-20 17:31:11 +0300

    Update Source1.cpp.cpp

--- Source1.cpp.cpp
+++ Source1.cpp.cpp
@@ -2,42 +2,42 @@
 #include <cmath>
 using namespace std;
 /**
-*  \brife Функция расчкта по заданной формуле.
-*  \param x Аргумент функции.
-*  \param y Аргумент функции.
-*  \return Звнчение функции.
+*  \brife Р¤СѓРЅРєС†РёСЏ СЂР°СЃС‡РєС‚Р° РїРѕ Р·Р°РґР°РЅРЅРѕР№ С„РѕСЂРјСѓР»Рµ.
+*  \param x РђСЂРіСѓРјРµРЅС‚ С„СѓРЅРєС†РёРё.
+*  \param y РђСЂРіСѓРјРµРЅС‚ С„СѓРЅРєС†РёРё.
+*  \return Р—РІРЅС‡РµРЅРёРµ С„СѓРЅРєС†РёРё.
 */
 const double getA(const double x, const double y, const double z);
 /**
-*  \brife Функция расчкта по заданной формуле.
-*  \param x Аргумент функции.
-*  \param zАргумент функции.
-*  \return Звнчение функции.
+*  \brife Р¤СѓРЅРєС†РёСЏ СЂР°СЃС‡РєС‚Р° РїРѕ Р·Р°РґР°РЅРЅРѕР№ С„РѕСЂРјСѓР»Рµ.
+*  \param x РђСЂРіСѓРјРµРЅС‚ С„СѓРЅРєС†РёРё.
+*  \param zРђСЂРіСѓРјРµРЅС‚ С„СѓРЅРєС†РёРё.
+*  \return Р—РІРЅС‡РµРЅРёРµ С„СѓРЅРєС†РёРё.
 */
 const double getB(const double x, const double y, const double z);
 /**
-* \brief Точка входа в программу.
-* \return Возращает 0 в случае успеха.
+* \brief РўРѕС‡РєР° РІС…РѕРґР° РІ РїСЂРѕРіСЂР°РјРјСѓ.
+* \return Р’РѕР·СЂР°С‰Р°РµС‚ 0 РІ СЃР»СѓС‡Р°Рµ СѓСЃРїРµС…Р°.
 */
 int main()
 {
-	const double x = 1.4;
-	const double y = 3.1;
-	const double z = 0.5;
-	const double a = getA(x, y, z);
-	const double b = getB(x, y, z);
+    const double x = 1.4;
+    const double y = 3.1;
+    const double z = 0.5;
+    const double a = getA(x, y, z);
+    const double b = getB(x, y, z);
 
-	cout << " x = " << x << " y = " << y << " z = " << z << "\n";
-	cout << " a = " << a << " b = " << b << endl;
-	return 0;
+    cout << " x = " << x << " y = " << y << " z = " << z << "\n";
+    cout << " a = " << a << " b = " << b << endl;
+    return 0;
 }
 
 const double getA(const double x, const double y, const double z)
 {
-	return sqrt(z * x * sin(2 * x) + exp(-2 * x) * (x + y));
+    return sqrt(z * x * sin(2 * x) + exp(-2 * x) * (x + y));
 }
 
 const double getB(const double x, const double y, const double z)
 {
-	return (exp(2 * x) * log(z + x) - pow(y, 3 * x) * log(y - x));
+    return (exp(2 * x) * log(z + x) - pow(y, 3 * x) * log(y - x));
 }
